ffac0d8a8a3670ecbdd8480c416c36b6884abb52
Stephanie <stephanie.walker@sada.com>
2021-07-19 11:10:03 -0700

    all

---
+++ app-2/src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Items() {
+    return (
+        <div className="each-item">
+        <input type="checkbox" />
+        </div>
+    )
+}
+
+export default Items
---
+++ app-2/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FrontPage from './components/FrontPage'
+
+
+
+ReactDOM.render(
+
+<FrontPage />,
+ document.getElementById("root")
+ );
