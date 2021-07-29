3e434e9106f5e80b3971cc60af386bd49c1446fb
Stephanie <stephanie.walker@sada.com>
2021-07-29 15:17:23 -0700

    finished project

--- react-5/app-5/src/memeProject/Memes.js
+++ react-5/app-5/src/memeProject/Memes.js
@@ -7,18 +7,60 @@ class Meme extends React.Component {
 		this.state = {
 			topText: "",
 			bottomText: "",
-			randomImg: "http://imgflip.com/1bij.jpg"
+			randomImg: "http://i.imgflip.com/1bij.jpg",
+			allMemes: []
 		}
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
 	componentDidMount() {
 		fetch("https://api.imgflip.com/get_memes")
-		.then()
+		.then(response => response.json()) 
+		.then(response => {
+			const {memes} = response.data
+			this.setState({allMemes: memes})
+		})
+	}
+
+	handleChange(event) {
+		const {name, value} = event.target
+		this.setState ({
+			[name] : value
+		})
+	}
+
+	handleSubmit(event){
+		event.preventDefault()
+		const randomNum = Math.floor(Math.random() * this.state.allMemes.length)
+		const randomMeme = this.state.allMemes[randomNum].url
+
+		this.setState({ randomImg: randomMeme })
+
 	}
 	
 	render() {
 		return(
-			<h1>Memes!</h1>
+			<div>
+				<form className="meme-form" onSubmit={this.handleSubmit}> 
+
+					<input type="text" name="topText" value={this.state.topText} onChange={this.handleChange} placeholder="Top Text"/>
+					<input type="text" name="bottomText" value={this.state.bottomText} onChange={this.handleChange} placeholder="Bottom Text"/>
+
+				<button>Generate</button>
+				</form>
+				{/* 
+				this bootom area will make it possible to display the memes that we are receving
+				from the api call and then placing in the array
+				 */}
+
+				 <div className="meme">
+					 <img src={this.state.randomImg} alt="meme"/>
+					 <h2 className="top">{this.state.topText}</h2>
+					 <h2 className="bottom">{this.state.bottomText}</h2>
+
+				 </div>
+			</div>
 		)
 	}
 
